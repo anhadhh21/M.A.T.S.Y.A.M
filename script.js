272f48f8cd372703fa2a91cd5e7167236c29b199
sonalbaury <111610395+sonalbaury@users.noreply.github.com>
2023-10-07 06:14:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,36 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const passwordForm = document.getElementById("password-form");
+    const passwordCard = document.getElementById("password-card");
+    const passwordNameDisplay = document.getElementById("password-name-display");
+    const passwordDisplay = document.getElementById("password-display");
+
+    const addButton = document.getElementById("add-password-button");
+    addButton.addEventListener("click", function () {
+        // Get values from the form
+        const passwordName = document.getElementById("password-name").value;
+        const password = document.getElementById("password").value;
+
+        // Display the password card with the entered data
+        passwordNameDisplay.textContent = `Password Name: ${passwordName}`;
+        passwordDisplay.textContent = `Password: ${password}`;
+        passwordCard.classList.remove("hidden");
+
+        // Clear the form
+        passwordForm.reset();
+    });
+});
+
+
